8364f561d42bdd457df2e13fbe41e53531254ef6
beraqiamouad <berqiamouad@gmail.com>
2022-09-08 15:26:41 +0100

    improving the model using CNN and making a video detector using opencv

--- app.py
+++ app.py
@@ -1,29 +1,88 @@
-import tensorflow as tf
+from operator import mod
+import cv2
 import numpy as np
-import gradio as gr
-
-# importing the model 
-model = tf.keras.models.load_model('digit_classification.h5')
+import tensorflow as tf
 
 
-# A function to take an image and resize it, then classify it
-def classify_image(image):
-    img = image.reshape(-1, 28, 28)
-    image_size = img / 255.0
-    prediction = model.predict(image_size)
-    return np.argmax(prediction)
+def predict(model, img):
+    imgs = np.array([img])
+    res = model.predict(imgs)
+    index = np.argmax(res)
+    return str(index)
+
+# left mouse click handler 
+startInference = False
+def ifClicked(event, x, y, flags, params):
+    global startInference
+    if event == cv2.EVENT_LBUTTONDOWN:
+        startInference = not startInference
 
 
-# creating the interface
-my_app = gr.Interface(
-    classify_image, 
-    'sketchpad', 
-    'label', 
-    live="True",
-    title="Softmax Digit Classification"
-)
+# threshold slider handler 
+threshold = 100
+def on_threshold(x):
+    global threshold
+    threshold = x
 
 
-# here I launch the app, so when we execute this file the app will be created
-my_app.close()
-my_app.launch(debug="True", share="True")
+# the opencv display loop 
+def start_cv(model):
+    global threshold
+    cap = cv2.VideoCapture(0)
+    frame = cv2.namedWindow('background')
+    cv2.setMouseCallback('background', ifClicked)
+    cv2.createTrackbar('threshold', 'background', 150, 255, on_threshold)
+    background = np.zeros((480, 640), np.uint8)
+    frameCount = 0
+
+    while True:
+        ret, frame = cap.read()
+
+        if (startInference):
+            frameCount += 1
+
+            frame[0:480, 0:80] = 0
+            frame[0:480, 560:640] = 0
+            grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            _, thr = cv2.threshold(grayFrame, threshold, 255, cv2.THRESH_BINARY_INV)
+           
+            resizedFrame = thr[240-75:240+75, 320-75:320+75]
+            background[240-75:240+75, 320-75:320+75] = resizedFrame
+
+            iconImg = cv2.resize(resizedFrame, (28, 28))
+            
+            # pass to model predictor 
+            res = predict(model, iconImg)
+
+            if frameCount == 5:
+                background[0:480, 0:80] = 0
+                frameCount = 0
+
+            cv2.putText(background, res, (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 3)
+            cv2.rectangle(background, (320-80, 240-80), (320+80, 240+80), (255, 255, 255), thickness=3)
+            
+            cv2.imshow('background', background)
+        else:
+            cv2.imshow('background', frame)
+
+        if cv2.waitKey(1) & 0xff == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+
+# main function aka the app
+def main():
+    model = tf.keras.models.load_model('digit_classification.h5')
+    
+    print("openning the app...")
+
+    start_cv(model)
+
+
+
+if __name__ == '__main__':
+    main()
